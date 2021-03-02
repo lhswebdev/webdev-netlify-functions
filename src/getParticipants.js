582a3d1ad893a004ab4df5acab3454ffd18a5f0e
KentoNishi <kento24gs@outlook.com>
2021-03-02 13:06:25 -0800

    Fixed the error!

--- src/getParticipants.js
+++ src/getParticipants.js
@@ -11,7 +11,7 @@ const meetings = {
     }
 }; 
 
-exports.handler = async (event, context) {
+exports.handler = async (event, context) => {
     return {
         statusCode: 200,
         body: JSON.stringify({
